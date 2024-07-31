788ece2f076fa2d57699e7eb483c0f53c6373bfe
shingy <mukandiherzel@gmail.com>
2024-07-31 12:15:39 +0200

    works_needs_f

--- arithmetic_formatter/working_example.py
+++ arithmetic_formatter/working_example.py
@@ -41,11 +41,29 @@ def add_time(start, duration, day=False):
         new_day = day_of_week_array[index]
         return_time += ', ' + new_day
 
+
     if amount_of_days == 1:
-        print (return_time + ' ' + '(next day)')
+       print (return_time + ' ' + '(next day)')
     elif amount_of_days > 1:
-        print( return_time + ' (' + str(amount_of_days) + ' days later)')
+       print( return_time + ' (' + str(amount_of_days) + ' days later)')
     elif amount_of_days == 0:
-        print(return_time)
+       print(return_time)
 
 add_time('6:30 PM', '17:30', 'Monday')
+add_time('3:00 PM', '3:10')
+# Returns: 6:10 PM
+
+add_time('11:30 AM', '2:32', 'Monday')
+# Returns: 2:02 PM, Monday
+
+add_time('11:43 AM', '00:20')
+# Returns: 12:03 PM
+
+add_time('10:10 PM', '3:30')
+# Returns: 1:40 AM (next day)
+
+add_time('11:43 PM', '24:20', 'tueSday')
+# Returns: 12:03 AM, Thursday (2 days later)
+
+add_time('6:30 PM', '205:12')
+# Returns: 7:42 AM (9 days later)
